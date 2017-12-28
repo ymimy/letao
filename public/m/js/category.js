426c3d078d02a001ba12e4d0180b2103dd550171
yzh <abc@163.com>
2017-12-28 21:07:06 +0800

    第二次提交

--- public/m/js/category.js
+++ public/m/js/category.js
@@ -1,8 +1,109 @@
 /*
-* @Author: 姚展鸿
-* @Date:   2017-12-27 22:25:24
-* @Last Modified by:   姚展鸿
-* @Last Modified time: 2017-12-27 22:25:32
-*/
+ * @Author: zhengwei
+ * @Date:   2017-12-26 15:03:37
+ * @Last Modified by:   姚展鸿
+ * @Last Modified time: 2017-12-28 10:34:38
+ */
+$(function() {
+    
+    // 初始化mui的区域滚动 传入父容器的选择器
+    mui('.mui-scroll-wrapper').scroll({
+        scrollY: true, //是否竖向滚动
+        scrollX: false, //是否横向滚动
+        startX: 0, //初始化时滚动至x
+        startY: 0, //初始化时滚动至y
+        indicators: false, //是否显示滚动条
+        deceleration: 0.001, //阻尼系数,系数越小滑动越灵敏
+        bounce: true //是否启用回弹
+    });
+    getCategoryLeftData();
+    categoryLeftClick();
+    getCategoryRightData(1);
+});
+// 获取左侧的分类数据
+function getCategoryLeftData() { 
+     $.ajax({
+         url:'/category/queryTopCategory',
+         success:function (data) {
+            //  已经取到数据了
+            //  console.log(data);
+            //  调用template(模板id,数据)
+             var html = template('categoryLeftTmp',data);
+            //  把生成的模板html放到左侧分类页面 生成的是li反倒ul中
+             $(".motion ul").html(html);
+            //  给第一个li添加active类名
+            $('.motion ul li:eq(0)').addClass('active');
+           }
+     })
+  }
 
-'use strict';
+
+
+
+ 
+    /*1. 给左侧分类菜单添加点击事件
+    2. 在点击事件里面 获取到当前点击的元素
+    3. 调用右侧分类的接口 
+        接口地址 http://localhost:3000/category/querySecondCategory?id=1 
+        这个接口地址需要一个参数是分类菜单的id
+    4. 拿到当前点击元素对应的分类id 
+    5. 在渲染左侧分类菜单的模板的时候同时还要把分类id存起来 （自定义属性）
+    6. 通过自定义属性获取点击左侧分类菜单的分类id
+    7. 调用ajax发送请求右侧分类商品数据（把分类id作为参数传入）
+    8. 定义右侧分类商品的模板引擎 绑定需要渲染的属性
+    9. 调用template('模板id',数据)方法生成模板页面
+    10 把生成好的的模板页面放到右侧的分类商品容器里面 */
+    //给左侧分类菜单绑定点击事件的时候切记不能直接给里面的a绑定因为a是动态生成的
+//  获取右侧的数据
+function categoryLeftClick() {
+    // 给左边点击事件来获取右边数据
+    $('.motion').on('click','ul li a',function (e) {
+         console.log(e);
+        // 清空所有的li的active在给当前点击的a的父元素li添加一个active
+        $('.motion ul li').removeClass('active');
+        // target 当前意思this   再找父親增加active
+        $(e.target.parentNode).addClass('active');
+        var id = $(e.target).data('id'); 
+        // 调用数据的索引
+        getCategoryRightData(id);
+
+      })
+  }
+//   右侧请求数据 
+function getCategoryRightData(id) {
+   $.ajax({
+       url:'/category/querySecondCategory',
+       data:{'id':id},
+       success:function (data) {
+        //    console.log(data);
+           var html = template('categoryRightTmp',data);
+        //    判断如果有数据就添加html如果没有数据就添加提示没有数据
+        if (data.rows.length) {
+            $('.category-right .mui-scroll').html(html);
+        }else {
+            $('.category-right .mui-scroll').html('<p>没有数据了</p>')
+        }
+         
+           
+     }
+   })
+  }
+
+  /*
+  function getCategoryRightData(id) {
+    $.ajax({
+        url: '/category/querySecondCategory',
+        data: { 'id': id },
+        success: function(data) {
+            console.log(data);
+            var html = template('categoryRightTmp', data);
+            // 判断如果有数据就添加html如果没有数据添就添加提示没有数据
+            if (data.rows.length) {
+                $('.category-right .mui-scroll').html(html);
+            } else {
+                $('.category-right .mui-scroll').html('<p>没有数据</p>')
+            }
+        }
+    })
+}
+  */
--- public/m/js/search.js
+++ public/m/js/search.js
@@ -1,8 +1,120 @@
-/*
-* @Author: 姚展鸿
-* @Date:   2017-12-27 21:31:05
-* @Last Modified by:   姚展鸿
-* @Last Modified time: 2017-12-27 21:31:11
-*/
+ 
 
-'use strict';
+
+
+ $(function () {  
+     addHistory();
+     queryHistory();
+     deleteHistory();
+     clearHistory();
+ });
+
+
+//  添加历史记录
+function addHistory() {
+    // 1. 给搜索按钮添加事件
+    $('.btn-search').on('click',function () {
+        // 2获取输入框输入的搜索内容
+         
+        // console.log($('.search-input').val());
+
+        var search = $('.search-input').val();
+        // 如果搜索的内容为空就判断
+        if(!search){
+            alert('没有输入内容');
+            return;
+        }
+        // 3.把当前的搜索记录存储到本地存储
+            //  3.1获取本地存储的搜索记录
+            var historyData = localStorage.getItem('historyData')
+        //  判断本地储存的历史记录是否有值
+        if (historyData) {
+            // 有值就转换为数组
+            historyData = JSON.parse(historyData);
+        }else{
+            // 如果没有就赋值为空数组
+            historyData = [];
+        }
+        // 判断当前搜索的内容是否在数组里面存储(判断以前是否已经有了) 如果存在就返回当前存在的索引 不存在返回
+        if(historyData.indexOf(search) == -1) {
+            // 不存在就添加
+            // 把搜索的内容添加到历史记录数组里面
+            historyData.push(search);
+            // 把历史记录数组转成字符串存储到本地存储中的setItem('本地存储的键','当前的历史记录的值')localStorage.setItem是找到本地的存储
+            // JSON.stringify(转换)
+            localStorage.setItem('historyData', JSON.stringify(historyData));
+            // 添加了一次历史记录就查询一次 调用下面把下面存储好的记录显示在列表里
+            queryHistory();
+        }
+        //点击搜索清空输入框
+        $('.search-input').val("");
+      });
+  }
+
+
+//   查询历史记录 显示到历史记录列表里
+function queryHistory() {
+    // 1. 获取本地存储的搜索历史记录
+    // 2.把获取的搜索历史记录转换成数组, 如果为空就赋值为空数组
+    // 3.定义模板引擎生成记录模板
+    // 4.调用模板引擎生成html
+    // 5.把生成的模板放到页面上
+    //1.1获取本地存储的搜索历史记录(本地存储里面的值都是字符串)
+        //  没有值存储就为空,也可自定为historyData
+    var historyData = localStorage.getItem('historyData');
+    // 現在存儲的為空直接轉換數組為null轉不了,,需要判斷 (本地存儲都是字符串需轉)
+    // 判断本地存储的历史记录是否有值
+    if (historyData) {
+        // 有值就转成数组
+        historyData = JSON.parse(historyData);
+    }else {
+        // 如果没有值,就赋值为空数组
+        historyData = [];
+    }
+    // 如果要反转 就调用数组反转方法
+    historyData = historyData.reverse();
+    // 数据必须是一个对象
+    // template('searchHistoryTmp',数据)
+    // 如果是数组需要包装成一个对象才能调用模板引擎生成模板
+    var html = template('searchHistoryTmp', { 'rows': historyData});
+    // 把生成的模板放到页面上
+    $('.search-history-list ul').html(html);
+  }
+
+// 给X添加删除事件
+function deleteHistory() {
+    //1.给删除的x添加点击事件
+    // 因为删除按钮动态生成 ,需要使用委托绑定事件
+    $('.search-history-list').on('click','.btn-delete',function () {
+        // 获取当前的点击删除的历史记录
+        var historyData = $(this).parent().data('history');
+        // 获取本地存储的搜索历史记录 (本地存储里面的值都是字符串)
+        var historyData = localStorage.getItem('historyData');
+        // 判断本地存储的历史记录是否有值
+        if (historyData) {
+            // 有值就转成数组
+            historyData = JSON.parse(historyData);
+        }else {
+            // 如果没有值 就赋值为空数组
+            historyData = [];
+        }
+        var historyIndex = historyData.indexOf(history + "");
+        historyData.splice(historyIndex, 1)
+        // console.log(historyData);
+        // 删除完毕之后重新把数组保存到本地存储里面
+        localStorage.setItem('historyData', JSON.stringify(historyData));
+        // 删除了一次历史记录就查询一次
+        queryHistory();
+      })
+  }
+
+
+//   给删除全部事件
+function clearHistory() {
+    $('.btn-clear').on('click',function () {
+        // 直接把本地存储的historyData的值设置为空字符串
+        localStorage.setItem('historyData','');
+        // 删除了一次历史记录就查询一次
+        queryHistory();
+      })
+  }
